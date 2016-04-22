681c9cdb9531e9a1c73330decd047479028dc782
Ian Rose <ianrosedev@gmail.com>
2016-04-22 14:31:22 -0400

    Moved 'fortune cookie' functionality into module

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fortune = require('./lib/fortune.js');
 
 //set up handlebars view engine
 var handlebars = require('express-handlebars')
@@ -14,19 +15,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.render('home');
 });
-
-//Ch3 - Virtual fortune cookie
-var fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple"
-];
 app.get('/about', function(req, res) {
-  var randomFortune = fortunes[Math.floor(Math.random()
-    * fortunes.length)];
-  res.render('about', {fortune: randomFortune});
+  res.render('about', {fortune: fortune.getFortune()});
 }); 
 
 //404 catch-all handler (middleware)
